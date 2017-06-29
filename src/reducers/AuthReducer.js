999efc5f25130e3309637093759510a716527e98
Ryan <ryanhuebert@gmail.com>
2017-06-29 07:36:39 -0700

    Add LOGIN_USER_SUCCESS case to AuthReducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,14 +1,18 @@
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS
  } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
-  password: ''
+  password: '',
+  user: null
 };
 
 export default (state = INITIAL_STATE, action) => {
+  console.log(action);
+
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
@@ -16,6 +20,8 @@ export default (state = INITIAL_STATE, action) => {
       // ...state : take all values from state object
     case PASSWORD_CHANGED:
         return { ...state, password: action.payload };
+    case LOGIN_USER_SUCCESS:
+        return { ...state, user: action.payload };
     default:
       return state;
       // no change occurs, nothing updates
