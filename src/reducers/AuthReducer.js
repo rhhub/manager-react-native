54fd1075dc01cef8f9f517d964d215367f632599
Ryan <ryanhuebert@gmail.com>
2017-06-30 08:01:37 -0700

    Create LOGIN_USER_FAIL redux.

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,17 +1,19 @@
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
  } from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
   password: '',
-  user: null
+  user: null,
+  error: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
+  //console.log(action);
 
   switch (action.type) {
     case EMAIL_CHANGED:
@@ -22,6 +24,8 @@ export default (state = INITIAL_STATE, action) => {
         return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
         return { ...state, user: action.payload };
+    case LOGIN_USER_FAIL:
+        return { ...state, error: 'Authentication Failed.', password: '' };
     default:
       return state;
       // no change occurs, nothing updates
